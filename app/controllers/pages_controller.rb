4015d9ca53a2dae86a14573e17bb5098c1f67f28
Max Kelly <max@revelthat.com>
2019-10-22 20:06:37 +1100

    changed routing after successful login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,7 @@ class PagesController < ApplicationController
 
   end 
 
+  def user_login_stage
 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   get "/", to: "pages#index", as: "root"
 
+  get "/user_login_stage", to: "pages#user_login_stage", as: "user_login_stage"
   get "/parents", to: "parents#index", as: "parents_home"
 end

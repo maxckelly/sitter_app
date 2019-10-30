fce59d8dfc56fc48cdd97bb947311cf326ba3845
Max Kelly <max@revelthat.com>
2019-10-30 16:57:03 +1100

    added to seed document

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,4 +7,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+generated_password = Devise.friendly_token.first(8)
+
+
+Role.create!(
+  name: "parent"
+)
+
+Role.create!(
+  name: "sitter"
+)
+
+for i in 1..10
+  User.create!(
+    email: Faker::Internet.email,
+    password: "12345678",
+    role_id: 2,
+  )
+  puts "Created #{i} user"
+end
+
+
+# for i in 1..10
+#   Sitter.create(
+#     name: Faker::Name.name,
+#     description: Faker::Quote.famous_last_words,
+#     age: Faker::Date.between(from: 200.days.ago, to: Date.today),
+#     location: Faker::Address.city,
+#     user_id: 1
+#   )
+#   puts "Created #{i} sitters"
+# end
 

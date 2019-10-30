4737f1f4369f3ea76d161525df0006d372cd9ee7
Max Kelly <max@revelthat.com>
2019-10-30 20:37:22 +1100

    cleaned up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,6 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-generated_password = Devise.friendly_token.first(8)
-
-
 Role.create!(
   name: "parent"
 )

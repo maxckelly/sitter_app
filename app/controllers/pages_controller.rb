5742d57784c715894f69aa8e8c999929f6dcb965
Max Kelly <max@revelthat.com>
2019-10-30 11:14:19 +1100

    fixed navbar issue with image not being passed to the presignup/login page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController 
 
+  before_action :set_user
+
   def index
     @sitters = Sitter.all
   end 
@@ -7,4 +9,14 @@ class PagesController < ApplicationController
   def unauthorised
 
   end
+
+  private 
+
+    def set_user
+      if user_signed_in?
+        id = params[:id]
+        @parent = Parent.find_by_user_id(current_user.id)
+        @sitter = Sitter.find_by_user_id(current_user.id)
+      end
+    end
 end
